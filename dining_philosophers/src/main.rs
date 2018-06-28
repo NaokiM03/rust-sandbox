62c57b4c57ae6accf5baa2ded68154c6ed314e34
NaokiM03 <NaokiM03@example.com>
2018-06-28 22:39:16 +0900

    [update]multi thread

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -16,7 +16,7 @@ impl Philosopher {
         println!("{} is eating.", self.name);
 
         thread::sleep(Duration::from_millis(1000));
-        
+
         println!("{} is done eating", self.name);
     }
 }
@@ -30,7 +30,13 @@ fn main() {
         Philosopher::new("Michel Foucault"),
     ];
 
-    for p in &philosophers {
-        p.eat();
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        thread::spawn(move || {
+            p.eat();
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
     }
 }
