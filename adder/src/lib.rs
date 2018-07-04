449810302ca9c10c5bb56c26ebfe3107a06e9cd9
NaokiM03 <NaokiM03@example.com>
2018-07-04 18:09:35 +0900

    [add]tdd_test

---
+++ adder/src/lib.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+pub struct Dollar {
+    amount: u32
+}
+
+impl Dollar {
+    pub fn new(amount: u32) -> Dollar {
+        Dollar { amount: 10 }
+    }
+    pub fn times (&self, multiplier: u32) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_multiplication() {
+        let five = Dollar::new(5);
+        five.times(2);
+        assert_eq!(10, five.amount);
+    }
+}
