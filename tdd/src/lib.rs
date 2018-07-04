76bdc1480d739bd683d6d31553aef2ed089de4aa
NaokiM03 <NaokiM03@example.com>
2018-07-04 22:41:14 +0900

    [update]add Franc

--- tdd/src/lib.rs
+++ tdd/src/lib.rs
@@ -16,6 +16,23 @@ impl Dollar {
     }
 }
 
+pub struct Franc {
+    amount: u32
+}
+
+impl Franc {
+    pub fn new(amount: u32) -> Franc {
+        Franc { amount: amount }
+    }
+    pub fn times (&self, multiplier: u32) -> Franc {
+        Franc { amount: self.amount * multiplier }
+    }
+
+    pub fn equals (&self, target: Franc) -> bool {
+        self.amount == target.amount
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -29,6 +46,12 @@ mod tests {
     fn test_equality() {
         assert!(Dollar::new(5).equals(Dollar::new(5)));
         assert!(!Dollar::new(5).equals(Dollar::new(6)));
+    }
+    #[test]
+    fn test_franc_multiplication() {
+        let five = Franc::new(5);
+        assert!(Franc::new(10).equals(five.times(2)));
+        assert!(Franc::new(15).equals(five.times(3)));
     }
 
 }
