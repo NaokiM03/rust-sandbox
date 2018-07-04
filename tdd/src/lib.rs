b5e2c0308f08334fb0688545dca6f35177351851
NaokiM03 <NaokiM03@example.com>
2018-07-04 23:21:00 +0900

    [update]put goal_post

--- tdd/src/lib.rs
+++ tdd/src/lib.rs
@@ -39,9 +39,9 @@ mod tests {
     use super::*;
     #[test]
     fn test_multiplication() {
-        let five = Dollar::new(5);
-        assert!(Dollar::new(10).equals(five.times(2)));
-        assert!(Dollar::new(15).equals(five.times(3)));
+        let five = Money::dollar(5);
+        assert!(Money::dollar(10).equals(five.times(2)));
+        assert!(Money::dollar(15).equals(five.times(3)));
     }
     #[test]
     fn test_franc_multiplication() {
@@ -55,6 +55,7 @@ mod tests {
         assert!(!Dollar::new(5).equals(Dollar::new(6)));
         assert!(Franc::new(5).equals(Franc::new(5)));
         assert!(!Franc::new(5).equals(Franc::new(6)));
+        assert!(!Franc::new(5).equals(Dollar::new(5)));
     }
 
 }
