bfcdb86c3a383551f33b1e1ba884b14895565be8
NaokiM03 <NaokiM03@example.com>
2018-07-04 23:44:05 +0900

    [update]

--- tdd/src/lib.rs
+++ tdd/src/lib.rs
@@ -51,17 +51,9 @@ mod tests {
         assert!(Money::dollar(15).equals(five.times(3)));
     }
     #[test]
-    fn test_franc_multiplication() {
-        let five = Money::franc(5);
-        assert!(Money::franc(10).equals(five.times(2)));
-        assert!(Money::franc(15).equals(five.times(3)));
-    }
-    #[test]
     fn test_equality() {
         assert!(Money::dollar(5).equals(Money::dollar(5)));
         assert!(!Money::dollar(5).equals(Money::dollar(6)));
-        assert!(Money::franc(5).equals(Money::franc(5)));
-        assert!(!Money::franc(5).equals(Money::franc(6)));
         assert!(!Money::franc(5).equals(Money::dollar(5)));
     }
     #[test]
