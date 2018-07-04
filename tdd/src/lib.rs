ec1c09cf0490755a09917048b74cb83d421ee2d7
NaokiM03 <NaokiM03@example.com>
2018-07-04 23:42:47 +0900

    [update]add curreny

--- tdd/src/lib.rs
+++ tdd/src/lib.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 pub struct Money {
-    amount: u32
+    amount: u32,
+    currency: &'static str
 }
 
 trait MoneyTrait {
@@ -15,28 +16,28 @@ pub struct Franc {
 
 impl Money {
     pub fn times(&self, multiplier: u32) -> Money {
-        Money { amount:self.amount * multiplier }
+        Money {
+            amount: self.amount * multiplier,
+            currency: &self.currency
+        }
     }
     pub fn equals(&self, target: Money) -> bool {
-        self.amount == target.amount
+        self.amount == target.amount && self.currency == target.currency
     }
     pub fn dollar(amount: u32) -> Money {
-        Money { amount: amount }
+        Money {
+            amount: amount,
+            currency: "USD"
+        }
     }
     pub fn franc(amount: u32) -> Money {
-        Money { amount: amount }
+        Money {
+            amount: amount,
+            currency: "CHF"
+        }
     }
-}
-
-impl MoneyTrait for Dollar {
-    fn new(amount: u32) -> Money {
-        Money { amount: amount }
-    }
-}
-
-impl Franc {
-    pub fn new(amount: u32) -> Money {
-        Money { amount: amount }
+    pub fn currency(&self) -> &'static str {
+        self.currency
     }
 }
 
@@ -61,6 +62,11 @@ mod tests {
         assert!(!Money::dollar(5).equals(Money::dollar(6)));
         assert!(Money::franc(5).equals(Money::franc(5)));
         assert!(!Money::franc(5).equals(Money::franc(6)));
-        // assert!(!Money::franc(5).equals(Money::dollar(5)));
+        assert!(!Money::franc(5).equals(Money::dollar(5)));
+    }
+    #[test]
+    fn test_currency() {
+        assert_eq!("USD", Money::dollar(1).currency());
+        assert_eq!("CHF", Money::franc(1).currency());
     }
 }
