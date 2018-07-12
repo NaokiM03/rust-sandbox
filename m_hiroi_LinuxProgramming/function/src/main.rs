e10b091850da746c452e26d22db59589627c772e
NaokiM03 <NaokiM03@example.com>
2018-07-12 11:13:42 +0900

    [update] fibonacci number

--- m_hiroi_LinuxProgramming/function/src/main.rs
+++ m_hiroi_LinuxProgramming/function/src/main.rs
@@ -7,8 +7,24 @@ fn fact(n: i64) -> i64 {
     }
 }
 
+fn fibo(n: i64) -> i64 {
+    fn fiboiter(n: i64, a: i64, b: i64) -> i64 {
+        if n == 0 {
+            a
+        }
+        else {
+            fiboiter(n - 1, b, a + b)
+        }
+    }
+    fiboiter(n, 0, 1)
+}
+
 fn main() {
     for n in 10 .. 20 {
         println!("{}", fact(n));
+    }
+
+    for n in 40 .. 50 {
+        println!("{}", fibo(n));
     }
 }
