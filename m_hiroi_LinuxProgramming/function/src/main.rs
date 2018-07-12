a8064ac76f7519d2e6a343e93e32483d7c1baf68
NaokiM03 <NaokiM03@example.com>
2018-07-12 20:16:54 +0900

    [update] use local function

--- m_hiroi_LinuxProgramming/function/src/main.rs
+++ m_hiroi_LinuxProgramming/function/src/main.rs
@@ -19,12 +19,31 @@ fn fibo(n: i64) -> i64 {
     fiboiter(n, 0, 1)
 }
 
+fn sum_of(func: fn(i32) -> i32, seq: &[i32]) -> i32 {
+    let mut acc: i32 = 0;
+    for i in 0 .. seq.len() {
+        acc += func(seq[i]);
+    }
+    acc
+}
+
 fn main() {
     for n in 10 .. 20 {
         println!("{}", fact(n));
     }
 
+
     for n in 40 .. 50 {
         println!("{}", fibo(n));
     }
+
+
+    fn identity(x: i32) -> i32 { x }
+    fn square(x: i32) -> i32 { x * x }
+    fn cube(x: i32) -> i32 {x * x * x}
+
+    let seq: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    println!("{}", sum_of(identity, &seq));
+    println!("{}", sum_of(square, &seq));
+    println!("{}", sum_of(cube, &seq));
 }
