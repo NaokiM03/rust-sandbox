dc29b22a871c7463c00349b4c6e6556a99e7f4b7
NaokiM03 <NaokiM03@example.com>
2018-07-16 11:24:29 +0900

    [update] use wild_card

--- m_hiroi_LinuxProgramming/pattern_match/src/main.rs
+++ m_hiroi_LinuxProgramming/pattern_match/src/main.rs
@@ -4,9 +4,20 @@ fn fact(n: i64) -> i64 {
         x => x * fact(x - 1)
     }
 }
+fn fact_with_wild_card(n: i64) -> i64 {
+    match n {
+        0 => 1,
+        _ => n * fact_with_wild_card(n - 1)
+    }
+}
+
 
 fn main() {
     for n in 10 .. 20 {
         println!("{}", fact(n));
     }
+    for n in 10 .. 20 {
+        println!("{}", fact_with_wild_card(n));
+    }
+
 }
