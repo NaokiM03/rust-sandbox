094e712bd489ba3c8e771ce489b7063e0a7f0787
NaokiM03 <NaokiM03@example.com>
2018-07-16 12:35:08 +0900

    [update] 3D struct

--- m_hiroi_LinuxProgramming/struct_test/src/main.rs
+++ m_hiroi_LinuxProgramming/struct_test/src/main.rs
@@ -1,2 +1,14 @@
+struct Point3D {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
 fn main() {
+    let p1 = Point3D {
+        x: 0.0,
+        y: 1.0,
+        z: 2.0
+    };
+    println!("{},{},{}", p1.x, p1.y, p1.z);
 }
