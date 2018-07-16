d4d747c3c913b61c54b25616ccc2e9e676232a1f
NaokiM03 <NaokiM03@example.com>
2018-07-16 12:47:58 +0900

    [update] 3D distance

--- m_hiroi_LinuxProgramming/struct_test/src/main.rs
+++ m_hiroi_LinuxProgramming/struct_test/src/main.rs
@@ -10,6 +10,13 @@ struct Point {
     y: f64,
 }
 
+fn distance(p3: &Point3D, p4: &Point3D) -> f64 {
+    let dx = p3.x - p4.x;
+    let dy = p3.y - p4.y;
+    let dz = p3.z - p4.z;
+    (dx *dx + dy * dy + dz * dz).sqrt()
+}
+
 fn main() {
     let p1 = Point3D {
         x: 0.0,
@@ -25,4 +32,8 @@ fn main() {
 
     let Point {x: a, y: b} = Point {x: 1.0, y: 2.0};
     println!("{:?}", Point{x: a, y: b});
+
+    let p3 = Point3D {x:0.0, y:0.0, z:0.0};
+    let p4 = Point3D {x:10.0, y:10.0, z:10.0};
+    println!("{}", distance(&p3, &p4));
 }
