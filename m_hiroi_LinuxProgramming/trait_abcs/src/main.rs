b5aa313c83f721cb55608d1aa55cd35a3c08c35f
NaokiM03 <NaokiM03@example.com>
2018-07-20 17:32:51 +0900

    [update] Point の new メソッドを追加

--- m_hiroi_LinuxProgramming/trait_abcs/src/main.rs
+++ m_hiroi_LinuxProgramming/trait_abcs/src/main.rs
@@ -6,6 +6,12 @@ struct Point {
     x: f64, y: f64
 }
 
+impl Point {
+    fn new(x1: f64, y1: f64) -> Point {
+        Point { x: x1, y: y1 }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
