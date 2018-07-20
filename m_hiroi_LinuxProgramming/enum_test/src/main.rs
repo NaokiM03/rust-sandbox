7989aa46d86dc5a4eb2297d96bcdb4915ab22ea2
NaokiM03 <NaokiM03@example.com>
2018-07-20 12:08:21 +0900

    [update] enum match

--- m_hiroi_LinuxProgramming/enum_test/src/main.rs
+++ m_hiroi_LinuxProgramming/enum_test/src/main.rs
@@ -1,3 +1,21 @@
+enum Fruit  {
+    Apple, Banana, Grape, Orange
+}
+
+use Fruit::*;
+
+fn get_price(fruit: Fruit) -> i32 {
+    match fruit {
+        Apple => 200,
+        Banana => 150,
+        Grape => 300,
+        Orange => 100,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_price(Apple));
+    println!("{}", get_price(Banana));
+    println!("{}", get_price(Grape));
+    println!("{}", get_price(Orange));
 }
