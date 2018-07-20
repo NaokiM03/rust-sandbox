08c295693145cf0ad09ab74c2e0016b594d64b01
NaokiM03 <NaokiM03@example.com>
2018-07-20 13:55:26 +0900

    [update] 二点間の距離を計算するメソッドを追加
    immutable な参照

--- m_hiroi_LinuxProgramming/method/src/main.rs
+++ m_hiroi_LinuxProgramming/method/src/main.rs
@@ -6,6 +6,11 @@ impl Point {
     fn new(x1: f64, y1: f64) -> Point {
         Point { x: x1, y: y1 }
     }
+    fn distance(&self, p: &Point) -> f64 {
+        let dx = self.x - p.x;
+        let dy = self.y - p.y;
+        (dx * dx + dy * dy).sqrt()
+    }
 }
 
 fn main() {
