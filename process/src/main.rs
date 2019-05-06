ede06236baf7242d2b670ea2c66ab36e02e00801
NaokiM03 <mattsun.phone@gmail.com>
2019-05-06 23:33:13 +0900

    バイト列から文字列へ

--- process/src/main.rs
+++ process/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    use std::process::Command;
+
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(&["/C", "echo hello"])
+                .output()
+                .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("echo hello")
+                .output()
+                .expect("failed to execute process")
+    };
+
+    let hello = String::from_utf8(output.stdout.to_vec()).unwrap();
+
+    println!("{}", hello);
 }
