dae78813102af1d52358148d0d9ea9dd0a48731b
NaokiM03 <mattsun.phone@gmail.com>
2019-05-07 02:22:26 +0900

    serde_json の練習

--- json/src/main.rs
+++ json/src/main.rs
@@ -1,3 +1,46 @@
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    let input = r#"
+{
+  "if": {
+    "0": {
+      "lit": true
+    },
+    "1": {
+      "func_call": {
+        "0": "p",
+        "1": {
+          "lit": "hoge"
+        }
+      }
+    },
+    "2": {
+      "if": {
+        "0": {
+          "lit": false
+        },
+        "1": {
+          "func_call": {
+            "0": "p",
+            "1": {
+              "lit": "fuga"
+            }
+          }
+        },
+        "2": {
+          "func_call": {
+            "0": "p",
+            "1": {
+              "lit": "piyo"
+            }
+          }
+        }
+      }
+    }
+  }
+}"#;
+
+    let a: Value = serde_json::from_str(input).unwrap();
+    println!("{}", a["if"]["1"]["func_call"]["0"]);
 }
