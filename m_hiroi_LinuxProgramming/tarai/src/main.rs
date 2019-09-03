68e826180866a8e85885b45bbe004e3750833189
NaokiM03 <mattsun.phone@gmail.com>
2019-09-03 22:32:37 +0900

    fix

--- m_hiroi_LinuxProgramming/tarai/src/main.rs
+++ m_hiroi_LinuxProgramming/tarai/src/main.rs
@@ -5,7 +5,7 @@ fn tak(x: i32, y: i32, z: i32) -> i32 {
         tak(tak(x - 1, y, z), tak(y - 1, z, x), tak(z - 1, x, y))
     }
 }
-                                                                     
-fn main() {                                             
-    println!("{}", tak(24, 12, 0));                                         
+
+fn main() {
+    println!("{}", tak(24, 12, 0));
 }
