f46028d860655356039dd00549fe514c5973c495
NaokiM03 <mattsun.phone@gmail.com>
2019-11-09 23:52:35 +0900

    ADD reqwest example from official

---
+++ exercises/examples/reqwest.rs
@@ -0,0 +1,16 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    println!("GET https://www.rust-lang.org/");
+
+    let mut res = reqwest::get("https://www.rust-lang.org/")?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:?}", res.headers());
+
+    // copy the response body directly to stdout
+    std::io::copy(&mut res, &mut std::io::stdout())?;
+
+    println!("\n\nDone.");
+    Ok(())
+}
